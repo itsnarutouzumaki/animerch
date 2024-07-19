f04c44e5e26eaca5bc3243fa2a4e5ae4f52f112c
somenath singh <144797336+itsnarutouzumaki@users.noreply.github.com>
2024-07-19 21:11:20 +0530

    Cart page completed

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -6,9 +6,18 @@ function Cart() {
   return (
     <div className="cart_container">
       <CartItem/>
-
       <div className="total_amount">
-        
+        <div className="price_amount">
+        <p className="amount_title">
+          Amount: 
+        </p>
+        <p className="amount_number">
+        ₹ 1800
+        </p>
+        </div>
+        <button className="proceed">
+          Place Order
+        </button>
       </div>
     </div>
   )
