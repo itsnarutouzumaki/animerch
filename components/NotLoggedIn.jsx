c20b79d8cdc3c035be3879ce4fca8bbb3e556ead
somenath singh <144797336+itsnarutouzumaki@users.noreply.github.com>
2024-10-19 00:42:03 +0530

    adding profile for test

--- components/NotLoggedIn.jsx
+++ components/NotLoggedIn.jsx
@@ -3,6 +3,7 @@ import { FaRegUserCircle } from "react-icons/fa";
 import { Link } from 'react-router-dom';
 import { FiLogIn } from "react-icons/fi";
 import { SiGnuprivacyguard } from "react-icons/si";
+import { CgProfile } from "react-icons/cg";
 function NotLoggedIn() {
   return (
     <li className="navbar-item dropdown">
@@ -10,6 +11,7 @@ function NotLoggedIn() {
             <ul className="dropdown-menu">
               <li><Link to="/login" className="dropdown-link"><FiLogIn className='dropped_down_icon'/>Login </Link></li>
               <li><Link to="/register" className="dropdown-link"><SiGnuprivacyguard className='dropped_down_icon'/> Register </Link></li>
+              <li><Link to="/profile" className="dropdown-link"><CgProfile className='dropped_down_icon'/> Profile </Link></li>
             </ul>
     </li>
   )
